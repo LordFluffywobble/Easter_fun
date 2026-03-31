04434b56271920e51c19dd527e718180a8a4e461
LordFluffywobble <38191273+LordFluffywobble@users.noreply.github.com>
2026-03-31 08:13:51 +0100

    Added some lists and a dictionary

--- Program.cs
+++ Program.cs
@@ -17,9 +17,10 @@ class Program
         float   []floatArray    = [323423524, 2348402358032, 059805];
         decimal []decimalArray  = [324324234.34534636m, 23423453454.43635645764m];
         long    []longArray     = [234234, 235436456467, 4564354654, 3565675676];
-        var data = Heisann();
+        
 
         Console.WriteLine("Hello, World!");
+        var data = Heisann();
         Console.WriteLine($"This prints the string {data.LittleString}");
         Console.WriteLine($"This prints the int type with the value {data.nr}");
         Console.WriteLine($"This prints the double type with the value {data.dd}");
@@ -51,11 +52,22 @@ class Program
         }
      
 
-
+    List<int> intString = new List<int>();
+    intString.Add(1);
+    intString.Add(2);
+    foreach(int nr in intString)
+        {
+            Console.WriteLine(nr);
+        }
        
+    Dictionary<string, double> MyDict = new Dictionary<string, double>();
 
-       
-    
+       MyDict.Add("Bond", 007);  //Only prints 7
+       MyDict.Add("Born", 123);
+    foreach(var thing in MyDict)
+        {
+            Console.WriteLine($"{thing.Key}: {thing.Value}");
+        }
 
     // //Oppgave 2    
         Calculator Calc = new Calculator();
